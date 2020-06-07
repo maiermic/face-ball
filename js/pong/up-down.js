1cd388339d2fac51021f81849c31428f14cc0a4e
Michael Maier <maier1michael@gmail.com>
2020-06-07 18:46:18 +0200

    add multi player version of pong

--- js/pong/up-down.js
+++ js/pong/up-down.js
@@ -25,38 +25,54 @@ export class Pong {
     this.createDownsidePaddle();
     this.createBall();
     this.createScoreBoards();
+    this.addControls();
+    this.addLogic();
   }
 
   createUpsidePaddle() {
     this._upsidePaddle =
-      Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+      Crafty.e("Paddle, 2D, DOM, Color")
         .color('rgb(255,0,0)')
         .attr({
           x: (this._boardWidth - this._paddleConfig.width) / 2,
           y: this._paddleConfig.distanceToWall,
           w: this._paddleConfig.width,
           h: this._paddleConfig.height,
-        })
-        .multiway(this._paddleConfig.speed, {A: 180, D: 0});
+        });
   }
 
   createDownsidePaddle() {
     this._downsidePaddle =
-      Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+      Crafty.e("Paddle, 2D, DOM, Color")
         .color('rgb(255,255,0)')
         .attr({
           x: (this._boardWidth - this._paddleConfig.width) / 2,
           y: this._boardHeight - this._paddleConfig.distanceToWall,
           w: this._paddleConfig.width,
           h: this._paddleConfig.height,
-        })
-        .multiway(this._paddleConfig.speed, {LEFT_ARROW: 180, RIGHT_ARROW: 0});
+        });
+  }
+
+  addControls() {
+    this.addUpsidePaddleControls();
+    this.addDownsidePaddleControls();
+  }
+
+  addUpsidePaddleControls() {
+    this._upsidePaddle
+      .addComponent('Multiway')
+      .multiway(this._paddleConfig.speed, {A: 180, D: 0});
+  }
+
+  addDownsidePaddleControls() {
+    this._downsidePaddle
+      .addComponent('Multiway')
+      .multiway(this._paddleConfig.speed, {LEFT_ARROW: 180, RIGHT_ARROW: 0});
   }
 
   createBall() {
-    const game = this;
     this._ball =
-      Crafty.e("2D, DOM, Color, Collision")
+      Crafty.e("2D, DOM, Color")
         .color('rgb(0,0,255)')
         .attr({
           x: this._ballConfig.initialPosition.x,
@@ -65,43 +81,55 @@ export class Pong {
           h: this._ballConfig.height,
           dX: Crafty.math.randomInt(2, 5),
           dY: Crafty.math.randomInt(2, 5)
-        })
-        .bind('UpdateFrame', function () {
-          // hit left or right boundary
-          if (this.x <= 0 || this.x >= game._boardWidth - this.w) {
-            this.dX *= -1;
-          }
-
-          //hit floor or roof
-          if (this.y > game._boardHeight) {
-            this.y = game._ballConfig.initialPosition.y;
-            Crafty("LeftPoints").each(function () {
-              this.text(++this.points + " Points")
-            });
-          }
-          if (this.y < this.w) {
-            this.y = game._ballConfig.initialPosition.y;
-            Crafty("RightPoints").each(function () {
-              this.text(++this.points + " Points")
-            });
-          }
-
-          this.x += this.dX;
-          this.y += this.dY;
-        })
-        .onHit('Paddle', function () {
-          this.dY *= -1;
         });
   }
 
+  addLogic() {
+    const game = this;
+    this._ball
+      .addComponent('Collision')
+      .bind('UpdateFrame', function () {
+        // hit left or right boundary
+        if (this.x <= 0 || this.x >= game._boardWidth - this.w) {
+          this.dX *= -1;
+        }
+
+        //hit floor or roof
+        if (this.y > game._boardHeight) {
+          this.y = game._ballConfig.initialPosition.y;
+          Crafty("LeftPoints").each(function () {
+            this.text(++this.points + " Points")
+          });
+        }
+        if (this.y < this.w) {
+          this.y = game._ballConfig.initialPosition.y;
+          Crafty("RightPoints").each(function () {
+            this.text(++this.points + " Points")
+          });
+        }
+
+        this.x += this.dX;
+        this.y += this.dY;
+
+        game.onUpdateFrame();
+      })
+      .onHit('Paddle', function () {
+        this.dY *= -1;
+      })
+  }
+
   createScoreBoards() {
-    //Score boards
-    Crafty.e("LeftPoints, DOM, 2D, Text")
-      .attr({x: 20, y: 20, w: 100, h: 20, points: 0})
-      .text("0 Points");
-    Crafty.e("RightPoints, DOM, 2D, Text")
-      .attr({x: 20, y: this._boardHeight - 40, w: 100, h: 20, points: 0})
-      .text("0 Points");
+    this._leftPoints =
+      Crafty.e("LeftPoints, DOM, 2D, Text")
+        .attr({x: 20, y: 20, w: 100, h: 20, points: 0})
+        .text("0 Points");
+    this._rightPoints =
+      Crafty.e("RightPoints, DOM, 2D, Text")
+        .attr({x: 20, y: this._boardHeight - 40, w: 100, h: 20, points: 0})
+        .text("0 Points");
+  }
+
+  onUpdateFrame() {
   }
 }
 
