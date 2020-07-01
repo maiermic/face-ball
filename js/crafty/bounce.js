ea221c2c2bad9e32c3e6614f84cf481f1e43efa9
Michael Maier <maier1michael@gmail.com>
2020-07-01 21:20:19 +0200

    accelerate ball

--- js/crafty/bounce.js
+++ js/crafty/bounce.js
@@ -4,14 +4,18 @@ Crafty.c('Bounce', {
   },
   bounceLeft() {
     this.vx = -Math.abs(this.vx)
+    this.ax = -Math.abs(this.ax)
   },
   bounceRight() {
     this.vx = Math.abs(this.vx)
+    this.ax = Math.abs(this.ax)
   },
   bounceUp() {
     this.vy = -Math.abs(this.vy)
+    this.ay = -Math.abs(this.ay)
   },
   bounceDown() {
     this.vy = Math.abs(this.vy)
+    this.ay = Math.abs(this.ay)
   },
 })
